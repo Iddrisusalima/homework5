01b0f6458c38b6d51a9f96e26a83c5a399922c59
Iddrisusalima <iddrisusalima733@gmail.com>
2024-12-02 09:39:18 +0000

    adding all files

---
+++ src/style.js
@@ -0,0 +1,10 @@
+alert("Welcome to our homepaage")
+
+let name = prompt("what is your name")
+
+let age =prompt("what is your age")
+
+let email=prompt("what is your email address" )
+
+alert("Hi +nname" = "You are"  + age + "years old and we will keep in touch via email:")
+
